456aa1e7046aabdc075bb450e985d717668525f5
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-19 17:24:22 +0300

    fix: :bug: add s3 policy for dla-user

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
 locals {
+  full_project_name = "${var.environment}-${var.project_name}"
   tags = {
-    owner             = "Gleb Khaykin"
-    full_project_name = "${var.environment}-${var.project_name}"
+    owner        = "Gleb Khaykin"
+    project_name = local.full_project_name
   }
 }
 
@@ -10,13 +11,33 @@ locals {
 ####################################################################################################
 
 resource "aws_s3_bucket" "this" {
-  bucket = "${var.project_name}-bucket"
+  bucket = "${local.full_project_name}-bucket"
   tags   = merge({ name = "DLA-S3-Bucket" }, local.tags)
 }
 
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
+}
+
+resource "aws_iam_user_policy" "s3" {
+  name = "dla-s3-policy"
+  user = "dla-user"
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+  }
+  EOF
 }
 
 ####################################################################################################

364d66591d2819c6895933f62417550a3f256eab
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-21 14:48:16 +0300

    chore: add environment variables

---
+++ src/utils/cfg.py
@@ -0,0 +1,10 @@
+"""Configuration variables."""
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_S3_BUCKET_DEV = os.getenv("AWS_S3_BUCKET_DEV")

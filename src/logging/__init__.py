12ea268d94fa30eb3e632530016050fc41a306f9
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-22 15:28:42 +0300

    feat: logging basic config

---
+++ src/logging/__init__.py
@@ -0,0 +1,58 @@
+"""Logging configuration for training models."""
+
+import logging
+import logging.config
+import sys
+
+from rich.logging import RichHandler
+
+from ...src import cfg
+
+LOGS_PATH = cfg.BASE_PATH / "logs"
+LOGS_PATH.mkdir(parents=True, exists_ok=True)
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOGS_PATH / "info.log",
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOGS_PATH / "error.log",
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "loggers": {
+        "root": {
+            "handlers": ["console", "info", "error"],
+            "level": logging.INFO,
+            "propagate": True,
+        },
+    },
+}
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger("root")
+logger.handlers[0] = RichHandler(markup=True)

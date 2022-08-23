2a0b1ba5f53d28c3cac26c5b8797b2cc01301bc0
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-23 13:56:54 +0300

    refactor: all import

--- src/data/datasets/__init__.py
+++ src/data/datasets/__init__.py
@@ -1,1 +1,5 @@
 """Datasets for training models."""
+
+from .asr import CommonVoiceDataset, LibriSpeechDataset, LJSpeechDataset
+
+__all__ = ["CommonVoiceDataset", "LibriSpeechDataset", "LJSpeechDataset"]

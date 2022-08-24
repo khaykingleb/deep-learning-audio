a165fe2489bf7e0c1635a065038773d38ed978c4
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-24 15:05:15 +0300

    refactor: :hammer:

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,8 +1,5 @@
 """Plots for a Digital Singnal Processing visualization."""
 
-import typing as tp
-
-import librosa
 import matplotlib.pyplot as plt
 import torch
 
@@ -15,7 +12,6 @@ def plot_signal_transformation(
     ylabel: str,
     sample_rate: int,
     audio_size: int,
-    use_db_transition: tp.Optional[bool] = True,
 ) -> None:
     """Plot audio singnal transformation.
 
@@ -26,11 +22,7 @@ def plot_signal_transformation(
         ylabel (str): Label for y-axis.
         sample_rate (int): Sample rate for a digital signal.
         audio_size (int): Length of a digital signal.
-        use_db_transition (bool, optional): Whether to use power-to-db transition
-            according to the formula db = 10 * log_10(power) or not.
     """
-    if use_db_transition:
-        transform = librosa.power_to_db(transform)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)

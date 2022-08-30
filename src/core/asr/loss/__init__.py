c5deb387d7dfcb32381d1c9eb19c6b34f70970bb
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-30 16:07:17 +0300

    refactor: :hammer:

--- src/core/asr/loss/__init__.py
+++ src/core/asr/loss/__init__.py
@@ -1,13 +1,10 @@
-"""Connectionist Temporal Classification loss."""
+"""Loss functions for ASR."""
 
-import typing as tp
-
-import torch
 import torch.nn as nn
 
 
 class CTCLossWrapper(nn.CTCLoss):
-    """Wrapper around Pytorch CTCLoss."""
+    """Wrapper around Pytorch Connectionist Temporal Classification loss."""
 
     def __init__(
         self: "CTCLossWrapper",
@@ -19,28 +16,3 @@ class CTCLossWrapper(nn.CTCLoss):
             blank_idx (int): Index of the blank token.
         """
         super().__init__(blank=blank_idx)
-
-    def forward(
-        self: "CTCLossWrapper",
-        log_probs: torch.Tensor,
-        targets: torch.Tensor,
-        input_lengths: tp.List[int],
-        target_lengths: tp.List[int],
-    ) -> torch.Tensor:
-        """Computes CTC loss function.
-
-        Args:
-            log_probs (Tensor): Logarithmized probabilities of shape (input_length, batch_size, vocab_size).
-            targets (Tensor): Encoded texts of shape (batch_size, max_target_length).
-            input_lengths (List): Transformation lengths.
-            target_lengths (List): Encoded texts lengths.
-
-        Returns:
-            Tensor: CTC loss value.
-        """
-        return super().forward(
-            log_probs,
-            targets,
-            input_lengths,
-            target_lengths,
-        )

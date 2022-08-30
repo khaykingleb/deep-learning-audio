c36f97f376468eb89cbc5ffa3d38e19a0244be70
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-31 01:23:41 +0300

    fix: :bug: plot is saved to buffer now

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,5 +1,8 @@
 """Plots for a Digital Singnal Processing visualization."""
 
+import io
+import typing as tp
+
 import matplotlib.pyplot as plt
 import torch
 
@@ -12,8 +15,9 @@ def plot_signal_transformation(
     ylabel: str,
     sample_rate: int,
     audio_size: int,
-) -> None:
-    """Plot audio singnal transformation.
+    show_fig: tp.Optional[bool] = True,
+) -> io.BytesIO:
+    """Plots audio singnal transformation and saves it to a buffer.
 
     Args:
         transform (Tensor): Audio singnal transformation of shape (1, n_transform, input_lenght).
@@ -22,6 +26,10 @@ def plot_signal_transformation(
         ylabel (str): Label for y-axis.
         sample_rate (int): Sample rate for a digital signal.
         audio_size (int): Length of a digital signal.
+        show_fig (bool, optional): Whether to show the figure.
+
+    Returns:
+        BytesIO: Buffer.
     """
     plt.title(title)
     plt.xlabel(xlabel)
@@ -32,4 +40,10 @@ def plot_signal_transformation(
         aspect="auto",
         extent=[0, audio_size / sample_rate, 0, sample_rate / 2000],
     )
-    plt.grid()
+    plt.grid(False)
+    buffer = io.BytesIO()
+    plt.savefig(buffer, dpi=250, format="jpeg")
+    if not show_fig:
+        plt.close()
+    buffer.seek(0)
+    return buffer

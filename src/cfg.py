6fc9231bb182b6b03c4d5b6e040736d2eb82a58a
Gleb Khaykin <khaykingleb@gmail.com>
2022-09-06 19:41:19 +0300

    refactor: :hammer:

--- src/cfg.py
+++ src/cfg.py
@@ -7,6 +7,6 @@ BASE_DIR = Path(__file__).parent.parent.resolve()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_S3_BUCKET_DEV = os.getenv("AWS_S3_BUCKET_DEV")
+AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
 
 WANDB_API_KEY = os.getenv("WANDB_API_KEY")

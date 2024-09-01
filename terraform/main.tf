b83c56d38acc216e1eb3312d22b545f227bdcd2a
Gleb Khaykin <khaykingleb@gmail.com>
2024-09-01 16:01:39 +0400

    refactor: :hammer:

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  full_project_name = "${var.environment}-${var.project_name}"
-  availability_zone = "${var.region}a"
-  tags = {
-    owner        = "Gleb Khaykin"
-    project_name = local.full_project_name
-  }
-}

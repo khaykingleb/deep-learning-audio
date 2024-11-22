990270ca480e9129e7da7b80231cdb66f2c0c565
Gleb Khaykin <khaykingleb@gmail.com>
2024-11-22 22:10:34 +0000

    chore: pass tests

--- tests/collections/audio/dsp/augmentation_test.py
+++ tests/collections/audio/dsp/augmentation_test.py
@@ -15,7 +15,7 @@ def sample_waveform() -> torch.Tensor:
     )
 
 
-def test_audio_augmenter_no_augmentations(sample_waveform: torch.Tensor):
+def test_audio_augmenter_no_augmentations():
     """Test that an error is raised if no augmentations are enabled."""
     with pytest.raises(
         ValueError,
@@ -24,14 +24,12 @@ def test_audio_augmenter_no_augmentations(sample_waveform: torch.Tensor):
             "augmentation method should be enabled."
         ),
     ):
-        augmenter = AudioAugmenter(
+        AudioAugmenter(
             sample_rate=16000,
             use_sox_effects=False,
             use_room_reverberation=False,
             use_background_noise=False,
         )
-
-    augmenter(sample_waveform)
 
 
 def test_audio_augmenter_initialization():
